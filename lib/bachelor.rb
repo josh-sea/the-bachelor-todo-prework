213eafe0f7c47e3f8694750c22bd6c4c308cff6c
Joshua Cocciardi <joshua.cocciardi@gmail.com>
2018-11-14 09:46:11 +0000

    Done.

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,9 +1,27 @@
 def get_first_name_of_season_winner(data, season)
   # code here
+  data.each do |seasons, contestants|
+    contestants.each do |i|
+      i.each do |title, info|
+        if info == "Winner"
+           return i[:name].split(" ")[0]
+        end
+      end
+    end
+  end
 end
 
 def get_contestant_name(data, occupation)
   # code here
+    data.each do |seasons, contestants|
+    contestants.each do |i|
+      i.each do |title, info|
+        if info == occupation
+           return i[:name]
+        end
+      end
+    end
+  end
 end
 
 def count_contestants_by_hometown(data, hometown)
