59ae74bfc3e1e0acadb41793e32d4d12c80e6e77
Joshua Cocciardi <joshua.cocciardi@gmail.com>
2018-11-16 19:21:44 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -76,7 +76,7 @@ def get_average_age_for_season(data, season)
 #        binding.pry
       end
 #      binding.pry
-      return ((sum + 0.0)/ contestants.length)
+      return ((sum + 0.0)/ contestants.length).ceil
     end
   end
 end
